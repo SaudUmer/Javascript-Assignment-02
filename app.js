5f451ff934ef93c65fed1b824a28049709a34b4a
SaudUmer <saudmamdani827@gmail.com>
2023-04-07 02:17:39 +0500

    website code push on github

---
+++ app.js
@@ -0,0 +1,100 @@
+//<<<<<<<<<<<<<  First Task Of Book Examples >>>>>>>>>>>>>>
+
+// <<<<<<<<<<<<< else if >>>>>>>>>>>>>>>
+
+// var num1 = 20;
+// var num2 = 40;
+// var num3 = 60;
+
+// if (num1 == num2){
+//     alert("num1 is equal to num2")
+// }else if(num1 === num3){
+//     alert("num1 is less than num3")
+// }else{
+//     alert("Not allowed to proceed")
+// }
+
+// xxxxxxxxxx---------xxxxxxxxxxx-----------xxxxxxxxxx------------xxxxxxxxxxxx
+
+// <<<<<<<<<<<<< nested else if >>>>>>>>>>>>>>
+
+// var firstnum = 45;
+// var secondnum = 55;
+// var nonum = false;
+
+// if(firstnum == secondnum){
+//    if (nonum == true){
+//     alert("Allowed")
+//    }else {
+//     alert("Not Allowed")
+//    }
+// }else{
+//     alert("You are not allowed for Boarding")
+// }
+
+// xxxxxxxxxx---------xxxxxxxxxxx-----------xxxxxxxxxx------------xxxxxxxxxxxx
+
+// var age = prompt("Enter Your Age")
+
+// if (age >= 18){
+//     alert("Allowed")
+// }else{
+//     alert("Not Allowed! (You are not considered to be 18+)")
+// }
+
+// xxxxxxxxxx---------xxxxxxxxxxx-----------xxxxxxxxxx------------xxxxxxxxxxxx
+
+// <<<<<<<<<<<<<<< && >>>>>>>>>>>>>>>>>>>>
+
+// var per1 = 20;
+// var per2 = 20;
+// var per3 = true;
+
+// if(per1 == per2 && per3 == true){
+//     alert("Yahoo! Your visa is ready for visit to Dubai.")
+// }else{
+//     alert("Your visa is not ready till yet.")
+// }
+
+// <<<<<<<<<<<<<<< || >>>>>>>>>>>>>>>>>>>>
+
+// var numb1 = 45;
+// var numb2 = 85;
+// var numb3 = true;
+
+// if(numb1 == numb2 || numb3 == false){
+//     alert("Allow for boarding to fly.")
+// }else{
+//     alert("You are not allowed for boarding.")
+// }
+
+// xxxxxxxxxx---------xxxxxxxxxxx-----------xxxxxxxxxx------------xxxxxxxxxxxx
+
+// <<<<<<<<<< Arrays >>>>>>>>>>>
+
+// var socialmediaapps = ["FaceBook", "Instagram", "Snapchat", "WhatsApp"]
+
+// socialmediaapps[1] = "Twitter";
+
+// console.log(socialmediaapps)
+
+// console.log(socialmediaapps.length)
+
+// socialmediaapps.push("LinkedIn");
+// socialmediaapps.push("YouTube");
+
+// socialmediaapps.pop()
+// console.log(socialmediaapps)
+
+// xxxxxxxxxx---------xxxxxxxxxxx-----------xxxxxxxxxx------------xxxxxxxxxxxx
+
+//<<<<<<<<<<<<<  Second Task Of Percentage Calculator  >>>>>>>>>>>>>>
+
+var obtain = +prompt("Enter Your Obtained Marks")
+var total = +prompt("Enter Your Total Marks")
+
+var percentage = obtain/total * 100
+
+alert("The total percentage is " + percentage + "%" )
+
+// xxxxxxxxxx---------xxxxxxxxxxx-----------xxxxxxxxxx------------xxxxxxxxxxxx
